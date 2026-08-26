9b1c5b5a2908a1b45f885128631a44d0a829eb35
Cheetah-Group <cheetahsolutions2@gmail.com>
2026-08-26 12:07:27 +0530

    Add SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,19 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel';
-
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://www.cheetahgroups.in',
   output: 'server',
   adapter: vercel(),
+
+  server: {
+    host: true,
+  },
+
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
